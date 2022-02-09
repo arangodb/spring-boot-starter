504cbe7b88e47999eddf226ee7afaa86cc4f486a
Michele Rastelli <michele@arangodb.com>
2022-02-09 10:48:15 +0100

    migrated Health Indicator to Spring Boot 2.4

--- src/main/java/com/arangodb/springframework/boot/actuate/autoconfigure/ArangoHealthIndicatorAutoConfiguration.java
+++ src/main/java/com/arangodb/springframework/boot/actuate/autoconfigure/ArangoHealthIndicatorAutoConfiguration.java
@@ -1,16 +1,11 @@
-/**
- *
- */
 package com.arangodb.springframework.boot.actuate.autoconfigure;
 
 import java.util.Map;
 
-import org.springframework.boot.actuate.autoconfigure.health.CompositeHealthIndicatorConfiguration;
-import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
-import org.springframework.boot.actuate.autoconfigure.health.HealthIndicatorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.health.*;
+import org.springframework.boot.actuate.health.HealthContributor;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -23,28 +18,20 @@ import com.arangodb.springframework.core.ArangoOperations;
 
 /**
  * @author Mark Vollmary
- *
+ * @author Michele Rastelli
  */
-@Configuration
-@ConditionalOnClass({ ArangoOperations.class, HealthIndicator.class, HealthIndicatorAutoConfiguration.class })
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({ ArangoOperations.class, HealthIndicator.class, HealthContributorAutoConfiguration.class })
 @ConditionalOnBean(ArangoOperations.class)
 @ConditionalOnEnabledHealthIndicator("arango")
-@AutoConfigureBefore(HealthIndicatorAutoConfiguration.class)
 @AutoConfigureAfter(ArangoAutoConfiguration.class)
 public class ArangoHealthIndicatorAutoConfiguration
-		extends CompositeHealthIndicatorConfiguration<ArangoHealthIndicator, ArangoOperations> {
-
-	private final Map<String, ArangoOperations> operations;
-
-	public ArangoHealthIndicatorAutoConfiguration(final Map<String, ArangoOperations> operations) {
-		super();
-		this.operations = operations;
-	}
+		extends CompositeHealthContributorConfiguration<ArangoHealthIndicator, ArangoOperations> {
 
 	@Bean
 	@ConditionalOnMissingBean(name = "arangoHealthIndicator")
-	public HealthIndicator arangoHealthIndicator() {
-		return createHealthIndicator(operations);
+	public HealthContributor arangoHealthIndicator(Map<String, ArangoOperations> operations) {
+		return createContributor(operations);
 	}
 
 }
