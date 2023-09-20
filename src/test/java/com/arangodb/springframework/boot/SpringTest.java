937c77aa1aee1ab5ce4d7324c7142ff97c22bffb
Arne Burmeister <arne.burmeister@wps.de>
2023-09-20 10:16:12 +0200

    #22 add test failing without event publisher in resolver

--- src/test/java/com/arangodb/springframework/boot/SpringTest.java
+++ src/test/java/com/arangodb/springframework/boot/SpringTest.java
@@ -21,21 +21,35 @@
 package com.arangodb.springframework.boot;
 
 import com.arangodb.springframework.annotation.Document;
+import com.arangodb.springframework.annotation.Ref;
 import com.arangodb.springframework.boot.actuate.ArangoHealthIndicator;
 import com.arangodb.springframework.core.ArangoOperations;
+import com.arangodb.springframework.core.mapping.event.AfterLoadEvent;
+import com.arangodb.springframework.core.mapping.event.AfterSaveEvent;
+import com.arangodb.springframework.core.mapping.event.ArangoMappingEvent;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Michele Rastelli
+ * @author Arne Burmeister
  */
 @SpringBootTest
 @EnableAutoConfiguration
@@ -50,6 +64,17 @@ public class SpringTest {
 
     @Autowired
     private CatRepo repo;
+
+    @Autowired
+    private MouseRepo referencingRepo;
+
+    @Autowired
+    private Listener listener;
+
+    @BeforeEach
+    void clearEvents() {
+        listener.events.clear();
+    }
 
     @Test
     public void operationsShouldBeNotNull() {
@@ -75,15 +100,94 @@ public class SpringTest {
         assertThat(read.id).isNotNull();
         assertThat(read.name).isEqualTo(cat.name);
     }
+
+    @Test
+    void event() {
+        Cat cat = new Cat();
+        cat.name = "Tom";
+        Cat saved = repo.save(cat);
+        assertThat(listener.events).hasSize(3)
+                .describedAs("event after save of cat")
+                .anyMatch(event -> matches(event, AfterSaveEvent.class, saved));
+        Mouse mouse = new Mouse();
+        mouse.huntBy = cat;
+        referencingRepo.save(mouse);
+        listener.events.clear();
+
+        Cat hunter = referencingRepo.findById(mouse.id).orElseThrow().huntBy;
+        assertThat(hunter).isEqualTo(saved);
+        assertThat(listener.events).describedAs("event after resolving cat")
+                .anyMatch(event -> matches(event, AfterLoadEvent.class, hunter));
+    }
+
+    @TestConfiguration
+    static class EventConfig {
+        @Bean
+        ApplicationListener<ArangoMappingEvent<?>> listener() {
+            return new Listener();
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static boolean matches(ArangoMappingEvent<?> event, Class<? extends ArangoMappingEvent> type, Object source) {
+        return type.isInstance(event) && event.getSource().equals(source);
+    }
 }
 
 interface CatRepo extends CrudRepository<Cat, String> {
 }
 
+interface MouseRepo extends CrudRepository<Mouse, String> {
+}
 
 @Document
 class Cat {
     @Id
     String id;
     String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(id, cat.id) && Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
+@Document
+class Mouse {
+    @Id
+    String id;
+
+    @Ref
+    Cat huntBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mouse mouse = (Mouse) o;
+        return Objects.equals(id, mouse.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
+class Listener implements ApplicationListener<ArangoMappingEvent<?>> {
+
+    final List<ArangoMappingEvent<?>> events = new ArrayList<>();
+
+    @Override
+    public void onApplicationEvent(@NonNull ArangoMappingEvent<?> event) {
+        events.add(event);
+    }
 }
