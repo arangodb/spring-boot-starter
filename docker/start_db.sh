f63fd574db7d1b6409fd557b00a6982e79711663
Michele Rastelli <michele@arangodb.com>
2024-01-16 15:23:50 +0100

    spring data v4.0.1

--- docker/start_db.sh
+++ docker/start_db.sh
@@ -4,7 +4,7 @@
 #   STARTER_MODE:             (single|cluster|activefailover), default single
 #   DOCKER_IMAGE:             ArangoDB docker image, default docker.io/arangodb/arangodb:latest
 #   SSL:                      (true|false), default false
-#   DATABASE_EXTENDED_NAMES:  (true|false), default false
+#   EXTENDED_NAMES:  (true|false), default false
 #   ARANGO_LICENSE_KEY:       only required for ArangoDB Enterprise
 
 # EXAMPLE:
@@ -13,9 +13,11 @@
 STARTER_MODE=${STARTER_MODE:=single}
 DOCKER_IMAGE=${DOCKER_IMAGE:=docker.io/arangodb/arangodb:latest}
 SSL=${SSL:=false}
-DATABASE_EXTENDED_NAMES=${DATABASE_EXTENDED_NAMES:=false}
+EXTENDED_NAMES=${EXTENDED_NAMES:=false}
 
 STARTER_DOCKER_IMAGE=docker.io/arangodb/arangodb-starter:latest
+GW=172.28.0.1
+docker network create arangodb --subnet 172.28.0.0/16
 
 # exit when any command fails
 set -e
@@ -32,10 +34,10 @@ AUTHORIZATION_HEADER=$(cat "$LOCATION"/jwtHeader)
 STARTER_ARGS=
 SCHEME=http
 ARANGOSH_SCHEME=http+tcp
-COORDINATORS=("172.17.0.1:8529" "172.17.0.1:8539" "172.17.0.1:8549")
+COORDINATORS=("$GW:8529" "$GW:8539" "$GW:8549")
 
 if [ "$STARTER_MODE" == "single" ]; then
-  COORDINATORS=("172.17.0.1:8529")
+  COORDINATORS=("$GW:8529")
 fi
 
 if [ "$SSL" == "true" ]; then
@@ -44,8 +46,8 @@ if [ "$SSL" == "true" ]; then
     ARANGOSH_SCHEME=http+ssl
 fi
 
-if [ "$DATABASE_EXTENDED_NAMES" == "true" ]; then
-    STARTER_ARGS="${STARTER_ARGS} --all.database.extended-names-databases=true"
+if [ "$EXTENDED_NAMES" == "true" ]; then
+    STARTER_ARGS="${STARTER_ARGS} --all.database.extended-names=true"
 fi
 
 if [ "$USE_MOUNTED_DATA" == "true" ]; then
@@ -66,9 +68,10 @@ docker run -d \
     $STARTER_ARGS \
     --docker.container=adb \
     --auth.jwt-secret=/jwtSecret \
-    --starter.address=172.17.0.1 \
+    --starter.address="${GW}" \
     --docker.image="${DOCKER_IMAGE}" \
-    --starter.local --starter.mode=${STARTER_MODE} --all.log.level=debug --all.log.output=+ --log.verbose
+    --starter.local --starter.mode=${STARTER_MODE} --all.log.level=debug --all.log.output=+ --log.verbose \
+    --all.server.descriptors-minimum=1024 --all.javascript.allow-admin-execute=true
 
 
 wait_server() {
@@ -87,12 +90,15 @@ done
 
 set +e
 for a in ${COORDINATORS[*]} ; do
+    echo ""
+    echo "Setting username and password..."
     docker run --rm ${DOCKER_IMAGE} arangosh --server.endpoint="$ARANGOSH_SCHEME://$a" --server.authentication=false --javascript.execute-string='require("org/arangodb/users").update("root", "test")'
 done
 set -e
 
 for a in ${COORDINATORS[*]} ; do
     echo ""
+    echo "Requesting endpoint version..."
     curl -u root:test --insecure --fail "$SCHEME://$a/_api/version"
 done
 
